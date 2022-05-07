f021afaef1fef85c0851cc3623f4ceb1f093c1f0
MC1R <Coughlinj17@gmail.com>
2022-05-07 19:52:58 -0400

    Add files via upload
    - Returned to using a while loop to make the camera work properly
    - Added logging

--- rockerCam.py
+++ rockerCam.py
@@ -8,11 +8,13 @@ Version: v.05
 
 import argparse
 from typing import NamedTuple
-import os, time, shutil
+import os, time, shutil, logging
 import cv2 #Needs pip
 import imutils #Needs pip
 import usb.core #Needs pip
 from threading import Thread
+
+
 
 class Args(NamedTuple):
     """ Command-line arguments """
@@ -64,6 +66,13 @@ def get_args() -> Args:
 
     args = parser.parse_args()
 
+    if args.duration == 0 or args.interval == 0:
+        raise Exception('Zeroes not allowed for duration or interval')
+
+    if os.path.isdir(args.directory):
+        print("Directory exists: Overwrite (y/n)")
+        shutil.rmtree(args.directory) if input() == 'y' else exit()
+
     return Args(args.directory,
                 args.interval,
                 args.duration,
@@ -81,66 +90,89 @@ def main() -> None:
     duration = args.duration
     interval = args.interval
 
+    # loggin
+    if args.debug:
+        global logger
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(levelname)s:%(funcName)s:%(threadName)s:%(message)s")
+        file_handler = logging.FileHandler('rockerLogs.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
     # Find the number of Cameras attached
     num_Cams = len(list(usb.core.find(find_all=True,idVendor=0x32e4)))
+    if num_Cams == 0:
+        raise Exception('Zero cameras detected')
 
     # Create a main directory and sub directories for each camera
-    if os.path.isdir(directory):
-        print("Directory already exists: Exiting...")
-        exit()
-    else:
-        os.mkdir(directory)
-        for i in range(num_Cams):
-            os.mkdir(f"{directory}/camera_{str(i +1)}")
+    os.mkdir(directory)
+    for i in range(num_Cams):
+        os.mkdir(f"{directory}/camera_{str(i +1)}")
+    logger.debug(f"Created {directory} in {os.getcwd()} and created {num_Cams} camera folders")
 
     # Function to open picture taking threads
     openCams(directory, num_Cams, duration, interval)
+    print("All pictures taken")
 
     # Time lapse the image sets
     if args.videoMode:
-        print("Condensing images to video")
         for camera in os.listdir(directory):
             img2video(directory, camera, args.videoFrames)
+        print("Output video saved")
 
 # --------------------------------------------------
 def openCams(directory, num_Cams, duration, interval):
     """ Wrapper for capture loop """
-    print("Starting to take pictures")
+    print("Starting to take pictures...")
     threads = []
 
     # Opens a thread for each camera with a target function of captureLoop
     for i in range(num_Cams):
         t = Thread(target=captureLoop, args=(f"{directory}/camera_{i+1}", duration, interval, (i * 2)))
+        logger.debug(f"Started thread for camera {i + 1}")
         t.start()
         threads.append(t)
 
     # Join threads
     for i in threads:
         i.join()
-    return print("All Pictures Taken")
+    logger.debug(f"Total of {(duration // interval) * num_Cams} pictures taken")
+    return
 
 # --------------------------------------------------
 def captureLoop(directory, duration, interval, camera_index):
-    """ Picture taking loop """
+    ''' Defines the loop to take pictures '''
 
-    # Define the number of pictures to take and initialize which image is being taken
+    # Define the number of pictures needed and find the times where each picture should be taken
     images = int(duration/interval)
+    pic_time = [x * interval for x in list(range(images + 1))]
+
+    # Intialize the count and img_num for saving
+    count = 0
     img_num = 0
 
-    # Opens the VideoCapture object
+    # Open the VideoCapture object
     cap = cv2.VideoCapture(camera_index + cv2.CAP_V4L2)
+    logger.debug(f"VideoCapture object opened as {cap}")
 
-    # Save an image and wait for the interval before saving again
-    for image in range(images):
-        _, frame = cap.read()
-        file_path = f"{directory}/capture_{img_num}.png"
+    while img_num <= images:
+        _, frame = cap.read() # Important to continously update to get correct interval
+        # Checks if a picture needs to taken
+        if count == pic_time[img_num]:
+            _, frame = cap.read()
+            file_path = f"{directory}/capture_{img_num}.png"
+            cv2.imwrite(file_path, frame)
+            img_num += 1
 
-        cv2.imwrite(file_path, frame)
+        # Wait 1 second and check conditions again
+        time.sleep(1)
+        count += 1
 
-        img_num += 1
-        time.sleep(interval)
+    # Delete zeroth picture because its typically taken before autofocus
+    os.remove(f"{directory}/capture_0.png")
 
-    # Close the VideoCapture object
+    # Release VideoCapture object
     cap.release()
     cv2.destroyAllWindows()
     return
@@ -151,13 +183,14 @@ def img2video(directory, camera, fps):
     path = os.path.join(directory, camera)
     os.chdir(path)
 
+    # Find and sort images into a list
     images = []
     for f in os.listdir():
         if f.endswith('.png'):
             images.append(f)
 
     images.sort(key=lambda x: os.path.getmtime(x))
-
+    logger.debug(f"Found {len(images)} images to be turned into time lapse from {camera}")
 
     # Determine the width and height from the first image
     frame = cv2.imread(images[0])
@@ -177,7 +210,7 @@ def img2video(directory, camera, fps):
     out.release()
     cv2.destroyAllWindows()
     os.chdir('../..')
-    print(f"The output video saved as {output}")
+    logger.debug(f"The output video saved as {output}")
     return
 
 # --------------------------------------------------
